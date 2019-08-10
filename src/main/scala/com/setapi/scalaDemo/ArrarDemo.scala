48360bae3feaaeafbdb30e4de75f9305f2a9e8c7
ShellMount <xieyi@qq.com>
2019-08-11 02:25:47 +0800

    可变数组的处理及常用的数组

--- src/main/scala/com/setapi/scalaDemo/ArrarDemo.scala
+++ src/main/scala/com/setapi/scalaDemo/ArrarDemo.scala
@@ -1,5 +1,7 @@
 package com.setapi.scalaDemo
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   *
   * Created by ShellMount on 2019/8/11
@@ -22,6 +24,35 @@ object ArrarDemo {
 
     // TODO: 转换数据类型: 父类转换为了类，得到更清晰的类型
     val intValue = arr3(0).asInstanceOf[Int]
+
+    // TODO: 可变数组：允许添加或删除元素
+    val arrBuffer = ArrayBuffer[Int]()
+
+    // 添加元素
+    arrBuffer += 3
+    arrBuffer += 5
+    arrBuffer += 7
+    arrBuffer += 9
+
+    println(s"arrBuffer= ${arrBuffer}")
+    arrBuffer(0) = 113
+    println(s"arrBuffer= ${arrBuffer}")
+
+    // 添加一组元素
+    arrBuffer ++= Array(11, 13, 15)
+    println(s"arrBuffer= ${arrBuffer}")
+
+    /**
+      * 在实际 中，将可变数组，转换为不可变数组，然后进行计算与操作
+      */
+    val arr4 = arrBuffer.toArray
+    arr4.foreach(println)
+
+    // TODO: 常用的makeString
+    println(arr4.mkString(", "))
+    println(arr4.mkString("<", ", ", ">"))
+
+
 
 
   }

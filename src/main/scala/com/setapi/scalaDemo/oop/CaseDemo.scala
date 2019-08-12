bd50e1778d3dc5143abd7e6b304523fe67665868
ShellMount <xieyi@qq.com>
2019-08-12 09:13:11 +0800

    CaseDemoEnhance

--- src/main/scala/com/setapi/scalaDemo/oop/CaseDemo.scala
+++ src/main/scala/com/setapi/scalaDemo/oop/CaseDemo.scala
@@ -8,21 +8,24 @@ package com.setapi.scalaDemo.oop
 
 object CaseDemo {
 
-  def judgeGrade(grade: String) = {
+  def judgeGrade(grade: String, name: String) = {
     grade match {
       case "A" => println("Excellent")
       case "B" => println("Good")
       case "C" => println("Just so so")
-      case _ => println("You need to work harder")
+      case _ if "zhangshan".equals(name) => println("Come on...")
+      case _grade => println(s"${name} grade is ${grade}, you need to work hard")
+
 
     }
   }
   def main(args: Array[String]): Unit = {
 
-    judgeGrade("A")
-    judgeGrade("B")
-    judgeGrade("C")
-    judgeGrade("E")
+    judgeGrade("A", "")
+    judgeGrade("B", "")
+    judgeGrade("C", "")
+    judgeGrade("E", "zhangshan")
+    judgeGrade("E", "xxx")
 
   }
 }

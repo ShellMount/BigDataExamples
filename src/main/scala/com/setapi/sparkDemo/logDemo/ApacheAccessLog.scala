3d384921afaaeeec38e309ebb12a5d95482f16ad
ShellMount <xieyi@qq.com>
2019-08-25 22:12:19 +0800

    Spark官方日志分析案例

---
+++ src/main/scala/com/setapi/sparkDemo/logDemo/ApacheAccessLog.scala
@@ -0,0 +1,55 @@
+package com.setapi.sparkDemo.logDemo
+
+import scala.util.matching.Regex
+
+/**
+  *
+  * Created by ShellMount on 2019/8/25
+  *
+  **/
+
+case class ApacheAccessLog (
+ipAddress: String,
+clientIdented: String,
+userId: String,
+dateTime: String,
+method: String,
+endpoint: String,
+protocol: String,
+resposeCode: Int,
+contentSize: Long)
+
+object ApacheAccessLog {
+  // 正则表达式, 以^开头，以$结束
+  // 1.1.1.1 - - [21/Jul/2014:10:00:00 -0800] "GET /chapter1/java/src/main/java/com/databricks/apps/logs/LogAnalyzer.java HTTP/1.1" 200 1234
+  val PARTTERN: Regex ="""^(\S+) (-|\S+) (-|\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+) (\S+)" (\d{3}) (\d+)$""".r
+
+  /**
+    * 用于解析LOG文件，将每行数据解析成对应的CASE CLASS
+    * @param log
+    * @return
+    */
+  def parseLogLine(log: String): ApacheAccessLog = {
+    // 使用正则表达式进行匹配
+    val res: Option[Regex.Match] = PARTTERN.findFirstMatchIn(log)
+
+    if(res.isEmpty) {
+      throw new RuntimeException(s"Cannot parse log line: ${log}")
+    }
+
+    // 获取值
+    val m: Regex.Match = res.get
+
+    ApacheAccessLog(
+      m.group(1),
+      m.group(2),
+      m.group(3),
+      m.group(4),
+      m.group(5),
+      m.group(6),
+      m.group(7),
+      m.group(8).toInt,
+      m.group(9).toLong
+    )
+  }
+}
---
+++ src/main/scala/com/setapi/sparkDemo/logDemo/LogAnalyzerSpark.scala
@@ -0,0 +1,106 @@
+package com.setapi.sparkDemo.logDemo
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  *
+  * Created by ShellMount on 2019/8/25
+  *
+  **/
+
+object LogAnalyzerSpark {
+  // 日志设置
+  Logger.getRootLogger.setLevel(Level.WARN)
+  val logger = Logger.getLogger(this.getClass)
+
+  def main(args: Array[String]): Unit = {
+    // 创建SparkConf
+    val sparkConf = new SparkConf()
+      .setAppName("LogAnalyzerSpark")
+      .setMaster("local[2]")
+
+    // 创建 SparkContext
+    val sc = SparkContext.getOrCreate(sparkConf)
+
+    /**
+      * 数据读取与业务逻辑
+      */
+    // val logFile = "file:///D:\\BaiduNetdiskDownload\\Spark从入门到上手实战\\0000.代码+软件+笔记+课件\\4、Spark Core（二）\\4、Spark Core（二）\\05_数据\\apache.access.log"
+    val logFile = "file:///D:\\BaiduNetdiskDownload\\Spark从入门到上手实战\\0000.代码+软件+笔记+课件\\4、Spark Core（二）\\4、Spark Core（二）\\05_数据\\access_log"
+    val accessLogsRDD = sc
+      // 读取文件
+      .textFile(logFile)
+      // 解析数据，将数据封装起来
+      .map(log => ApacheAccessLog.parseLogLine(log))
+
+    // 缓存数据
+    accessLogsRDD.persist(StorageLevel.MEMORY_AND_DISK)
+
+    println(s"Count: ${accessLogsRDD.count()} \n ${accessLogsRDD.first()}")
+
+
+    /**
+      * 需求1：计算content size 的最大值，最小值，平均值
+      */
+    val contentSizeRDD: RDD[Long] = accessLogsRDD.map(_.contentSize)
+    contentSizeRDD.cache()
+
+    // 计算
+    val avgContentSize = contentSizeRDD.reduce(_ + _) / contentSizeRDD.count()
+    val minContentSize = contentSizeRDD.min()
+    val maxContentSize = contentSizeRDD.max()
+
+    // 释放内存
+    contentSizeRDD.unpersist()
+
+    println(s"avgContentSize = ${avgContentSize}, minContentSize = ${minContentSize}, maxContentSize = ${maxContentSize}")
+
+    /**
+      * 需求2：返回结果的统计
+      */
+    val responseCodeToCount = accessLogsRDD
+      // 转换字段
+      .map(log => (log.resposeCode, 1))
+      // 聚合统计
+      .reduceByKey(_ + _)
+      // 由于Response Code的数量不多可以直接返回
+      .collect()
+
+    println(s"Response Code Count : ${responseCodeToCount.mkString("[", ", ", "]")}")
+
+    /**
+      * 需求3：统计IP地址的数量大于N次的
+      */
+    val ipAddress: Array[(String, Int)] = accessLogsRDD
+      .map(log => (log.ipAddress, 1))
+      .reduceByKey(_ + _)
+      // 访问网站超过20次的IP
+      .filter(_._2 > 0)
+      .take(20)
+
+    println(s"IP Address Access Count: ${ipAddress.mkString("[", ", ", "]")}")
+
+
+    /**
+      * 需求4： 求endpoint统计的TOPN
+      */
+    val topEndpointRDD = accessLogsRDD
+        .map(log => (log.endpoint, 1))
+        .reduceByKey(_ + _)
+        .sortBy(_._2)
+        .take(10)
+
+    println(s"endpoint sorted: ${topEndpointRDD.mkString("[", ", ", "]")}")
+
+
+    /**
+      * 程序结束
+      */
+    accessLogsRDD.unpersist()
+    Thread.sleep(10000 * 1000)
+    sc.stop()
+  }
+}
